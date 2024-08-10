012cde01f3701c04647e44a2494ebae418deeccf
JamieLoLo <bleach7408@hotmail.com>
2024-08-10 13:50:51 +0800

    fix database connection

--- models/index.js
+++ models/index.js
@@ -1,32 +1,33 @@
 'use strict'
-
+require('dotenv').config()
 const fs = require('fs')
+
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
-const env = process.env.NODE_ENV || 'development'
-const config = require(path.resolve(__dirname, '../config/config.json'))[env]
 const db = {}
+const env = process.env.NODE_ENV || 'development'
 
-// 資料庫連線
-let sequelize
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
+let config = require('../config/config.json')[env]
+
+config = {
+  ...config,
+  username: process.env.RDS_USERNAME || config.username,
+  password: process.env.RDS_PASSWORD || config.password,
+  host: process.env.RDS_HOST || config.host,
+  database: process.env.RDS_DB_NAME || config.database
+}
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
     dialect: 'mysql',
     dialectModule: require('mysql2')
-  })
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-
-    {
-      dialect: 'mysql',
-      dialectModule: require('mysql2')
-    }
-  )
-}
+  }
+)
 
 // 動態引入其他 models
 fs.readdirSync(__dirname)
