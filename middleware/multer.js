c8353020f0c200ed93291e28df26bcd7e325e774
JamieLoLo <bleach7408@hotmail.com>
2024-08-10 17:52:58 +0800

    fix image upload

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,3 +1,9 @@
+// const multer = require('multer')
+// const upload = multer({ dest: 'temp/' })
+// module.exports = upload
+
 const multer = require('multer')
-const upload = multer({ dest: 'temp/' })
+const memoryStorage = multer.memoryStorage() // 使用 MemoryStorage
+const upload = multer({ storage: memoryStorage })
+
 module.exports = upload
